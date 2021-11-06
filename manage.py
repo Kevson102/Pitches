169d48c8fb582ea2aa6fd9619a24fb75a87a8c68
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-11-06 11:10:23 +0300

    IMPORTS FLASK IN THE INIT.PY FILE TO RESOLVE THE ERRORS THAT THE APPLICATION IS SPITTING

---
+++ manage.py
@@ -0,0 +1,17 @@
+from app import create_app, db
+from flask_script import Manager, Server
+from app.models import User, Comment, Pitch
+
+# Creating app instances
+# app = create_app('development')
+app = create_app()
+
+manager = Manager(app)
+manager.add_command('server', Server)
+
+@manager.shell
+def make_shell_context():
+  return dict(app = app, db=db, User = User, Comment = Comment, Pitch = Pitch)
+
+if __name__ == '__main__':
+  manager.run()
