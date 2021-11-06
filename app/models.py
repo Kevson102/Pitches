844d692e999d6db4f43a274e0725c78818c7ad5a
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-11-06 13:46:18 +0300

    CREATES A TEST CASE FOR THE USER CLASS AND STARTS THE CREATION OF THE AUTH PACKAGE

--- app/models.py
+++ app/models.py
@@ -23,6 +23,17 @@ class User(db.Model):
   pitches = db.relationship('Pitch', backref='user', lazy="dynamic")
   comments = db.relationship('Comment', backref='user', lazy="dynamic")
   
+  @property
+  def password(self):
+    raise AttributeError('You are not authorized to read password attribute')
+  
+  @password.setter
+  def password(self, password):
+    self.pass_secure = generate_password_hash(password)
+    
+  def verify_password(self, password):
+    return check_password_hash(self.pass_secure, password)
+  
   def __repr__(self):
     return f'User {self.username}'
   
