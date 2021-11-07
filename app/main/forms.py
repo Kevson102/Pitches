ef9bb613910f4a4083d56e26dc7603e0a9021625
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-11-07 07:55:37 +0300

    UPDATES THE FORMS.PY FILE TO INCLUDE THE BLUEPRINTS OF THE PITCH FORM, THE USERPROFILE FORM, AND THE COMMENT ENTRY FORM

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,12 +5,16 @@ from wtforms.validators import Required
 from app.models import Pitch
 
 class PitchForm(FlaskForm):
-  pitch_category = SelectField('Pitch category', choices=[('pickup-lines', 'Pickup Lines'), ('interview-pitch', 'Interview Pitch'), ('product-pitch', 'Product Pitch'), ('promotion-pitch', 'Promotion Pitch')], validators=[Required()])
-  # StringField('Pitch category', validators=[Required()])
-  pitch_message = StringField('One minute pitch', validators=[Required()])
-  upvotes = IntegerField('Upvotes')
-  downvotes = IntegerField('Downvotes')
+  pitch_category = SelectField('Select Pitch Category', choices=[('pickup-lines', 'Pickup Lines'), ('interview-pitch', 'Interview Pitch'), ('product-pitch', 'Product Pitch'), ('promotion-pitch', 'Promotion Pitch')], validators=[Required()])
+  pitch_title = StringField('Title of the pitch', validators=[Required()])
+  pitch_author = StringField('Author', validators=[Required()])
+  pitch_message = TextAreaField('One minute pitch...', validators=[Required()])
   submit = SubmitField('Submit')
   
-class User(FlaskForm):
+class UserProfile(FlaskForm):
+  user_bio = TextAreaField('Enter Your Bio info Here...', validators=[Required()])
+  submit = SubmitField('Submit')
   
+class EnterCommentForm(FlaskForm):
+  comment = TextAreaField('Enter your comment here...', validators=[Required()])
+  submit = SubmitField('Submit')
