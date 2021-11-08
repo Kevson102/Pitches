92d667a896a00ac340d37d384cbbd332d5ea26fb
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-11-08 15:09:47 +0300

    IMPORVES ON THE FUNCTIONALITY OF SENDING WELCOME EMAILS TO THE USERS ON SIGN UP

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from . import auth
 from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
+from ..email import mail_message
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -28,9 +29,11 @@ def register():
     db.session.add(user)
     db.session.commit()
     
+    # mail_message("Welcome to Phoenix Pitches", "email/welcome_user", user.email, user = user)
+    
     return redirect(url_for('auth.login'))
   
-    # title = "New User"
+    title = "New Account"
   return render_template('auth/register.html', registration_form = form)
 
 @auth.route('logout')
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY='nexttimemseeh'
+export MAIL_USERNAME = kevson3941@gmail.com
+export MAIL_PASSWORD = Kevson@102
 
 python manage.py server
